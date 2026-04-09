091d3d71e9000d344f1f1c6aeb97e739cdad01e0
mayara <mayara.soliveiraifc@gmail.com>
2026-04-09 11:57:08 -0300

    FEAT(db.js): criando e importando db.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const db = require('./db');
 require('dotenv').config();
 
 const app = express();
@@ -7,11 +8,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/produtos', async (req, res) => {
+    try {
+        const [rows] = await db.query('SELECT * FROM Produto');
+        res.json(rows);
+    } catch (err) {
+        resizeTo.status(500).json({ erro: "Erro ao buscar produtos", detalhes: err });
+    }
+});
+
 app.get('/', (req, res) => {
     res.json({ mensagem: "Bem-vindo à API da Padaria Santo Martins!"});
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor voando na porta ${PORT}`);
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
